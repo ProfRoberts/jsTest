ebb3f881e58e276fc5c6e291357b1ce9ac2d62ed
profRoberts <rodger.roberts@gmail.com>
2021-10-14 20:28:45 -0500

    Add a multipication table

--- labTest.js
+++ labTest.js
@@ -24,4 +24,33 @@ function determineNumber(var1, var2){
 
 let solution = determineNumber(25, 57)
 
-console.log(solution);
+//console.log(solution);
+
+//Create a mulitplecation table
+
+//Create a varible that will change
+let result = 'x ';
+
+for(let n1 = 0; n1 <= 10; n1++){
+    
+    //Create a number
+    let num1 = n1;
+    for(let n2 = 0; n2 <= 10; n2++){
+        let num2 = n2;
+        let solution = n1 * n2;
+
+        if(n1 == 0 && n2 > 0){
+            result += '[' + n2 + ']';
+        }
+        else if(n2 == 0 && n1 > 0){
+            result += '[' + n1 + ']';
+        }
+        else if(n1 > 0 && n2 > 0){
+            result += (solution) + ' ';
+        }
+        
+    }
+    result += '\n';
+}
+
+console.log(result);
